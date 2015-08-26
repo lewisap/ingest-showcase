abaaafc3efb2c9d1a4e038cd85b9cdbac93550c9
Adam Lewis <adam.lewis@marklogic.com>
2015-08-26 10:35:43 -0400

    added stubbed test code for the java api

--- src/main/java/com/marklogic/isc/ApplicationConfiguration.java
+++ src/main/java/com/marklogic/isc/ApplicationConfiguration.java
@@ -5,17 +5,61 @@ import org.springframework.boot.autoconfigure.*;
 import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
 
+import com.marklogic.client.*;
+import com.marklogic.client.DatabaseClientFactory.Authentication;
+import com.marklogic.client.document.*;
+import com.marklogic.client.io.*;
+import com.marklogic.client.query.*;
+
 @RestController
 @EnableAutoConfiguration
 @SpringBootApplication
 public class ApplicationConfiguration {
 
-    @RequestMapping("/")
-    String home() {
-        return "Hello World!";
-    }
+  @RequestMapping("/")
+  String home() {
+    return "Hello World!";
+  }
 
-    public static void main(String[] args) throws Exception {
-        SpringApplication.run(ApplicationConfiguration.class, args);
-    }
+  @RequestMapping("/test")
+  String dbTest() {
+    DatabaseClient client = DatabaseClientFactory.newClient("localhost", 8000, "admin", "admin", Authentication.DIGEST);
+
+    // Make a document manager to work with text files.
+    TextDocumentManager docMgr = client.newTextDocumentManager();
+
+    // Define a URI value for a document.
+    String docId = "/example/text.txt";
+
+    // Create a handle to hold string content.
+    StringHandle handle = new StringHandle();
+
+    // Give the handle some content
+    handle.set("A simple text document");
+
+    // Write the document to the database with URI from docId
+    // and content from handle
+    docMgr.write(docId, handle);
+
+    client.release();
+    return "inserted a document?";
+  }
+
+  @RequestMapping("/search")
+  String search() {
+    StringBuffer buff = new StringBuffer();
+    DatabaseClient client = DatabaseClientFactory.newClient("localhost", 8000, "admin", "admin", Authentication.DIGEST);
+
+    QueryManager queryMgr = client.newQueryManager();
+    StringQueryDefinition qd = queryMgr.newStringDefinition();
+    qd.setCriteria("");
+    StringHandle results = queryMgr.search(qd, new StringHandle().withFormat(Format.JSON));
+
+    client.release();
+    return results.get();//buff.toString();
+  }
+
+  public static void main(String[] args) throws Exception {
+    SpringApplication.run(ApplicationConfiguration.class, args);
+  }
 }
